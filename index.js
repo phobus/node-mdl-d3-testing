e8b299ebdfab0dec548681c3e7893ca6f6a84570
phobus <immalloc@gmail.com>
2015-11-07 15:40:13 +0100

    fix error

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require("http");
 var fs = require("fs");
 var path = require("path");
-var mime = require("mime");
 
-var port_number = process.env.PORT || 5000);
+
+var port_number = process.env.PORT || 5000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {
